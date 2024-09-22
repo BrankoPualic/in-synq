685bd406f16fea8ed409074c0871281784e09fc4
Branko Pualić-Radujko <pualicbranko@gmail.com>
2024-09-22 13:02:34 +0200

    [FEAT] - Data - User seeds + Audit trigger stored procedure

---
+++ Application/Source/InSynq.Infrastructure.Data/Scripts/Procedures/usp_CreateAuditTriggerStoredProcedure.sql
@@ -0,0 +1,64 @@
+﻿CREATE PROCEDURE [dbo].[usp_CreateAuditTrigger] (@tableName VARCHAR(100), @excludedColumns VARCHAR(100) = '[]')
+AS
+BEGIN
+	DECLARE @sql VARCHAR(max)
+	DECLARE @insert_columns VARCHAR(max)
+	DECLARE @object_id INT
+	DECLARE @object_name VARCHAR(255)
+	DECLARE @triggerName VARCHAR(100)
+
+	SET @sql = ''
+	SET @insert_columns = ''
+
+	SELECT @object_id = OBJECT_ID(@tableName);
+	SELECT @object_name = OBJECT_NAME(@object_id);
+
+	DECLARE @exclude TABLE (value VARCHAR(100))
+
+	INSERT INTO @exclude SELECT value from openjson(CASE WHEN @excludedColumns IS NULL OR @excludedColumns = '' THEN '[]' ELSE @excludedColumns END)
+
+	SELECT @insert_columns = @insert_columns + '[' + name + '],' + CHAR(13) + CHAR(10) + CHAR(9) + CHAR(9) + CHAR(9) + CHAR(9)
+	FROM sys.columns
+	WHERE object_id = @object_id
+	AND name NOT IN (SELECT value FROM @exclude)
+	AND is_computed = 0
+	ORDER BY column_id;
+
+	SELECT @insert_columns = LEFT(@insert_columns, LEN(@insert_columns) - 7);
+
+	SET @triggerName = 'trg_Update_' + @object_name + 'Audit';
+
+	IF EXISTS(
+		SELECT 1 FROM sys.objects
+		WHERE type = 'TR'
+		AND name = @triggerName
+		AND parent_object_id = @object_id
+	)
+		SET @sql = 'ALTER'
+	ELSE
+		SET @sql = 'CREATE'
+
+	SET @sql = @sql + N' TRIGGER [' + OBJECT_SCHEMA_NAME(@object_id) + '].[' + @triggerName + '] ON [' + OBJECT_SCHEMA_NAME(@object_id) + '].[' + OBJECT_NAME(@object_id) + '] AFTER UPDATE AS ' + CHAR(13) + CHAR(10) + 'BEGIN' + CHAR(13) + CHAR(10);
+
+	SET @sql = @sql + N'
+	IF (SELECT COUNT(0)
+		FROM
+		(
+			SELECT ' + @insert_columns + '
+			FROM deleted d
+			EXCEPT
+			SELECT ' + @insert_columns + '
+			FROM inserted i
+		)x) > 0
+	BEGIN
+		INSERT INTO ' + OBJECT_SCHEMA_NAME(@object_id) + '.' + @object_name + '_aud (AuditTimeStamp, LogType, ' + @insert_columns + ')
+		SELECT SYSUTCDATETIME(), ''UPD'', ' + @insert_columns + '
+		FROM deleted
+	END
+END;';
+
+	--PRINT @sql
+	EXEC(@sql);
+
+END
+GO
---
+++ Application/Source/InSynq.Infrastructure.Data/Scripts/Seeds/User.sql
@@ -0,0 +1,116 @@
+﻿SET IDENTITY_INSERT [User] ON;
+
+-- Disable both foreign key constraints
+ALTER TABLE [User] NOCHECK CONSTRAINT FK_User_User_CreatedBy;
+ALTER TABLE [User] NOCHECK CONSTRAINT FK_User_User_LastChangedBy;
+
+INSERT INTO [User]
+(
+    Id,
+    FirstName,
+    MiddleName,
+    LastName,
+    Username,
+    Email,
+    Password,
+    ProfileImageUrl,
+    PublicId,
+    Biography,
+    DateOfBirth,
+    Details,
+    IsActive,
+    CreatedOn,
+    CreatedBy,
+    LastChangedOn,
+    LastChangedBy
+)
+VALUES
+(
+    1,
+    'System',
+    NULL,
+    'Admin',
+    'admin',
+    'sysadmin@insinq.com',
+    '$2a$12$ASR.eawVK67Rp6cXiul9zuYYYgmeosye4mLSEbqTj.lsunl8S63wi', --Pa$$w0rd
+    NULL,
+    NULL,
+    NULL,
+    '2002-10-10 00:00:00.000',
+    NULL,
+    1,
+    CURRENT_TIMESTAMP,
+    1,
+    CURRENT_TIMESTAMP,
+    1
+),
+(
+    2,
+    'System',
+    NULL,
+    'Member',
+    'member',
+    'sysmember@insinq.com',
+    '$2a$12$ASR.eawVK67Rp6cXiul9zuYYYgmeosye4mLSEbqTj.lsunl8S63wi', --Pa$$w0rd
+    NULL,
+    NULL,
+    NULL,
+    '2002-10-10 00:00:00.000',
+    NULL,
+    1,
+    CURRENT_TIMESTAMP,
+    1,
+    CURRENT_TIMESTAMP,
+    1
+),
+(
+    3,
+    'System',
+    NULL,
+    'UserAdmin',
+    'useradmin',
+    'sysuseradmin@insinq.com',
+    '$2a$12$ASR.eawVK67Rp6cXiul9zuYYYgmeosye4mLSEbqTj.lsunl8S63wi', --Pa$$w0rd
+    NULL,
+    NULL,
+    NULL,
+    '2002-10-10 00:00:00.000',
+    NULL,
+    1,
+    CURRENT_TIMESTAMP,
+    1,
+    CURRENT_TIMESTAMP,
+    1
+),
+(
+    4,
+    'System',
+    NULL,
+    'Moderator',
+    'moderator',
+    'sysmoderator@insinq.com',
+    '$2a$12$ASR.eawVK67Rp6cXiul9zuYYYgmeosye4mLSEbqTj.lsunl8S63wi', --Pa$$w0rd
+    NULL,
+    NULL,
+    NULL,
+    '2002-10-10 00:00:00.000',
+    NULL,
+    1,
+    CURRENT_TIMESTAMP,
+    1,
+    CURRENT_TIMESTAMP,
+    1
+);
+
+SET IDENTITY_INSERT [User] OFF;
+
+-- Re-enable the foreign key constraints
+ALTER TABLE [User] CHECK CONSTRAINT FK_User_User_CreatedBy;
+ALTER TABLE [User] CHECK CONSTRAINT FK_User_User_LastChangedBy;
+
+-- User Roles
+
+INSERT INTO UserRole
+(UserId,RoleId)
+VALUES
+(1,1), (2,2), (3,3), (4,4);
