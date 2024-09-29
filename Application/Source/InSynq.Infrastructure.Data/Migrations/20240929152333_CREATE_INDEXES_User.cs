4b6b68e712f7f3dc6c5c2c44e49e2bb236c2a803
Branko Pualić-Radujko <pualicbranko@gmail.com>
2024-09-29 16:07:32 +0200

    [MIGRATION] - Add code first indexes to table User

---
+++ Application/Source/InSynq.Infrastructure.Data/Migrations/20240929152333_CREATE_INDEXES_User.cs
@@ -0,0 +1,14 @@
+﻿using InSynq.Core.Model.Models.Application.User;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InSynq.Infrastructure.Data.Migrations;
+
+[DbContext(typeof(DatabaseContext))]
+[Migration("20240929152333_CREATE_INDEXES_User")]
+public partial class CreateUserIndexes : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.Up([User.IX_User_Username, User.IX_User_Email, User.IX_User_IsActive_IsLocked]);
+
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.Down([User.IX_User_Username, User.IX_User_Email, User.IX_User_IsActive_IsLocked]);
+}
